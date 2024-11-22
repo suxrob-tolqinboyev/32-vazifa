4de58beddf2db1f0fd80ec64434b96ec23607891
suxrob-tolqinboyev <tolqinboyevsuxrob053@gmail.com>
2024-11-23 02:46:28 +0500

    add LandingPage

---
+++ js/index.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+  theme: {
+    container: {
+      center: true,
+      width: {
+        tablet: "682px",
+        desktop: "1170px",
+      },
+      padding: {
+        default: "30px",
+        tablet: "0px",
+      },
+    },
+    screens: {
+      tablet: "780px",
+      desktop: "1280px",
+    },
+    extends: {
+      colors: {},
+    },
+  },
+};
